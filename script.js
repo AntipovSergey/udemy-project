b32fe8853337a444f70c905a3e4c41ea5f5e4f08
Antipov Sergey <antip99999@mail.ru>
2022-07-07 14:28:16 +0300

    Second exercise

--- script.js
+++ script.js
@@ -10,7 +10,27 @@ const personalMovieDB = {
   privat: false,
 };
 
-const lastWatchedFilm = prompt('Один из последних просмотренных фильмов?', '');
+for (let i = 0; i < 2; i++) {
+  const lastWatchedFilmFirstQuestion = prompt('Один из последних просмотренных фильмов?', '');
+  const yourRatingFirstQuestion = prompt('На сколько оцените его?', '');
 
-const yourRating = prompt('На сколько оцените его?', '');
+  if (lastWatchedFilmFirstQuestion !== null && yourRatingFirstQuestion !== null && lastWatchedFilmFirstQuestion !== '' && yourRatingFirstQuestion !== '' && lastWatchedFilmFirstQuestion.length < 50) {
+    personalMovieDB.movies[lastWatchedFilmFirstQuestion] = yourRatingFirstQuestion;
+    console.log('Done!')
+  } else {
+    console.log('Error!');
+    i--;
+  }
+}
 
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
